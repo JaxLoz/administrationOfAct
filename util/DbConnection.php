b148ce9df76c337e76140616cdf8d522c1d3fa9e
Javier E Montes <jaxloz2002@gmail.com>
2024-03-30 09:46:15 -0500

    cambio de base de datos

--- util/DbConnection.php
+++ util/DbConnection.php
@@ -10,10 +10,12 @@ class DbConnection
     private PDO $pdo;
     private static ?DbConnection $dbConnection = null;
 
+    private $host = "pgsql:host=ep-yellow-grass-a58n9hrr.us-east-2.aws.neon.fl0.io;port=5432;dbname=actReports;sslmode=require";
+
     public function __construct()
     {
         try {
-            $this->pdo = new PDO("mysql:host=localhost;dbname=minute_report", "root", "Cr@fteo12");
+            $this->pdo = new PDO("mysql:host=bisbz5bdtevegiq59lre-mysql.services.clever-cloud.com;port=3306;dbname=bisbz5bdtevegiq59lre", "uyeags95emzhznug", "aSh5riojFDJXDtWX8XHZ");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch (PDOException $e){
             die("Error al conectar la base de datos: ".$e);
